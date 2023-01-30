b8ac576d0e353ee8a395189f7afc4cec55a00198
Denis_Kormishkov <den00736@mail.ru>
2023-01-31 03:41:38 +0400

    Закоммитил все задачи на GitHUB

---
+++ Task004/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдает максимальное из этих чисел.
+// Например
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+
+// if (max < b)
+// {
+//   max = b;
+// }
+// else 
+// {
+//     max = a;
+// }
+
+// if (max<c)
+// {
+//     max = c;
+//     Console.WriteLine($"Максимальное число {max}");
+// }
+// else
+// {
+//     Console.WriteLine($"Максимальное число {max}");
+// }
+
+
+// Семён ради всего святого распишите, как эту задачу можно было сделать изящней и компактней. Заранее благодарю
+
+if (max < b)
+{
+    max =b;
+}
+else if (max < c)
+{
+    max = c;
+    Console.WriteLine($"Максимальное число {max}");
+}
+else 
+{
+    Console.WriteLine($"Максимальное число {max}");
+}
+
+// Вроде сделал. Тяжело быть тупым конечно.
